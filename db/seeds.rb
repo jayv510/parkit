b177328bd1f7bf3eadf39dca9a85813aa282a8fe
Chris Cheung <ckwcheung123@icloud.com>
2018-11-20 13:38:08 +0800

    seeds completed for users and spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing Database"
+Space.destroy_all
+
+puts "Generating Fake Database"
+
+puts "FIRST the USERS"
+
+User.create(
+  first_name: "Peter",
+  last_name: "Parker",
+  phone_number: Faker::PhoneNumber.cell_phone)
+
+User.create(
+  first_name: "Sam",
+  last_name: "Spaceown",
+  phone_number: Faker::PhoneNumber.cell_phone)
+
+puts "SECOND the SPACES"
+
+
+rate = [50, 100, 150]
+
+5.times do
+  Space.create(
+  title: Faker::Address.street_name,
+  description: "Great place to park in #{Faker::Address.street_name}",
+  address: Faker::Address.street_name,
+  half_hour_rate: rate.sample,
+  availability: true)
+end
+
+puts "DB for USERS and SPACES created!"
